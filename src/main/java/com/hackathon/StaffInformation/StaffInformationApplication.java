84ef7e6800cfb879b6f2171f396b09e8e04bd62e
Dhairya Shah <dhairyashah@Dhairyas-MacBook-Air.local>
2023-07-13 16:30:28 +0530

    basic setup

---
+++ src/main/java/com/hackathon/StaffInformation/StaffInformationApplication.java
@@ -0,0 +1,14 @@
+package com.hackathon.StaffInformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffInformationApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		SpringApplication.run(StaffInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/StaffInformation/StaffInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.StaffInformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
