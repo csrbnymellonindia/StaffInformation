f87dd4ece4d304398a8c5bc26aca972a4e8e5ee1
Balaji Anbalagan <vijibalaji2003@gmail.com>
2023-07-14 19:23:29 +0530

    login and favicon fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent, SuccessDialog } from './login/login.component';
 import { ActionCellRendererComponent, HomeComponent } from './home/home.component'
 import { ReactiveFormsModule } from '@angular/forms'
 import {MatIconModule } from '@angular/material/icon'
+import {MatDialogModule } from '@angular/material/dialog'
 import { MatButtonModule} from '@angular/material/button';
 import { AddStaffComponent } from './add-staff/add-staff.component';
 import { AuditLogComponent } from './audit-log/audit-log.component';
@@ -30,12 +31,14 @@ import { HttpClientModule } from '@angular/common/http';
     HomeComponent,
     AddStaffComponent,
     AuditLogComponent,
+    SuccessDialog,
     TasksComponent,
     ActionCellRendererComponent,
     LogoutComponent
   ],
   imports: [
     HttpClientModule,
+    MatDialogModule,
     BrowserModule,
     MatInputModule,
     MatMenuModule,
