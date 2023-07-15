b30b4436d685a3d4a28c78f3f705fe7203b7e7c1
Vidhi Vaswani <vidhi.vaswani123@gmail.com>
2023-07-15 13:05:17 +0530

    Junit Tests

---
+++ src/test/java/com/codeusingjava/controller/StudentControllerTest.java
@@ -0,0 +1,82 @@
+package com.codeusingjava.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.codeusingjava.model.StudentModel;
+import com.codeusingjava.service.StudentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+
+class StudentControllerTest {
+	
+	@Mock
+	StudentService studentService = mock(StudentService.class);
+	
+	@Autowired
+	MockMvc mockMvc;
+
+	private StudentModel studentModel = new StudentModel();
+	
+	private final ObjectMapper mapper = new ObjectMapper();
+	
+	
+	@BeforeEach
+	void setUp() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(new StudentController(studentService)).build();
+        studentModel.setAdditionalDetails("abc");
+        studentModel.setStudentIdentifier(123);
+	}
+	
+	@Test
+	void testaddStudent() throws Exception {
+		ResponseEntity<String> res = null ;
+		Mockito.when(studentService.addStudent(studentModel)).thenReturn(res);
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/students/addStud").content(this.mapper.writeValueAsBytes(studentModel));
+		mockMvc.perform(builder);
+	}
+	
+	@Test
+	void testupdateStudent() throws Exception {
+	ResponseEntity<String> res = null;
+	Mockito.when(studentService.updateStudent(123, studentModel)).thenReturn(res);
+	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/students/updateStud/123").content(this.mapper.writeValueAsBytes(studentModel));
+	mockMvc.perform(builder);
+	}
+	
+	@Test
+	void testdeleteStudent() throws Exception {
+		 MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.delete("/students/deleteStud/123");
+		   mockMvc.perform(builder);
+	}
+	
+	
+	
+	@Test
+	void testgetAllStudents() throws Exception {
+		Mockito.when(studentService.findAllStudents()).thenReturn(new ArrayList<>());
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/students/getAll");
+		mockMvc.perform(builder);
+	}
+	
+	@Test
+	void testgetStudentById() throws Exception {
+		Mockito.when(studentService.findAllStudents()).thenReturn(new ArrayList<>());
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/students/getStud/123");
+		mockMvc.perform(builder);
+	}
+
+}
---
+++ src/test/java/com/codeusingjava/controller/TeacherControllerTest.java
@@ -0,0 +1,135 @@
+package com.codeusingjava.controller;
+
+import static org.mockito.Mockito.mock;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.awt.PageAttributes.MediaType;
+import java.util.ArrayList;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.Mock;
+
+import org.mockito.Mockito;
+
+import org.mockito.MockitoAnnotations;
+
+import org.mockito.junit.MockitoJUnitRunner;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.codeusingjava.model.TeacherModel;
+
+import com.codeusingjava.service.TeacherService;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.hamcrest.Matchers;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+
+class TeacherControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@MockBean
+	TeacherService teacherService = mock(TeacherService.class);
+
+	private TeacherModel teacherModel = new TeacherModel();
+	
+	private final ObjectMapper mapper = new ObjectMapper();	
+
+	@BeforeEach
+	void setUp() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(new TeacherController(teacherService)).build();
+        teacherModel.setStaffId((long)123);
+        teacherModel.setAddress("abc");
+        teacherModel.setEmailId("xyz");
+        teacherModel.setPrimaryContactNumber((long)12364656);
+        teacherModel.setSecondaryContactNumber((long)79643);
+        teacherModel.setStaffName("abc");
+        teacherModel.setWhatsappNumber((long)12364656);
+        
+	}
+
+	@Test
+	void testfindAllTeachers() throws Exception {
+
+		Mockito.when(teacherService.findAllTeachers()).thenReturn(new ArrayList<>());
+
+		MockHttpServletRequestBuilder builder = get("/teacherDetails");
+
+		mockMvc.perform(builder);
+
+	}
+
+	@Test
+	void testfindById() throws Exception {
+
+		Optional<TeacherModel> value = Optional.ofNullable(new TeacherModel());
+
+		Mockito.when(teacherService.findById((long) 123)).thenReturn(value);
+
+		MockHttpServletRequestBuilder builder = get("/teacherDetails/123");
+
+		mockMvc.perform(builder);
+
+	}
+
+	@Test
+	void test() throws Exception {
+		ResponseEntity<String> res = null ;
+		Mockito.when(teacherService.updateTeacherDetails((long)123, teacherModel)).thenReturn(res);
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/updateTeacher/123").content(this.mapper.writeValueAsBytes(teacherModel));
+		mockMvc.perform(builder);
+ 
+	}
+	
+	@Test
+	void testAddTeacher() throws Exception {
+		ResponseEntity<String> res = null;
+		Mockito.when(teacherService.addTeacher(teacherModel)).thenReturn(res);
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/createTeacher").content(this.mapper.writeValueAsBytes(teacherModel));
+		mockMvc.perform(builder);
+	}
+	
+	@Test
+	void testdeleteTeacherDetails() throws Exception {
+	   MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.delete("/deleteTeacher/123");
+	   mockMvc.perform(builder);
+	}
+
+}
