a0cd8fffc707478b4a84fc9ce65c3d7f3ec4e1cc
Balaji Anbalagan <vijibalaji2003@gmail.com>
2023-07-16 20:22:38 +0530

    major change

--- src/main/java/com/codeusingjava/SpringJwtApplication.java
+++ src/main/java/com/codeusingjava/SpringJwtApplication.java
@@ -3,14 +3,29 @@ package com.codeusingjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
-public class SpringJwtApplication {
+public class SpringJwtApplication implements ErrorController {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJwtApplication.class, args);
+	}
+	
+	private static final String PATH = "/error";
+
+	@RequestMapping(value = PATH)
+	public String error(){
+		return "forward:/";
+	}
+	@Override
+	public String getErrorPath() {
+		return PATH;
 	}
 
 }
--- src/main/java/com/codeusingjava/controller/TeacherController.java
+++ src/main/java/com/codeusingjava/controller/TeacherController.java
@@ -1,7 +1,11 @@
 package com.codeusingjava.controller;
 
 import com.codeusingjava.model.TeacherModel;
+import com.codeusingjava.model.UserDao;
+import com.codeusingjava.service.JwtUserDetailsService;
 import com.codeusingjava.service.TeacherService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +17,9 @@ import java.util.Optional;
 public class TeacherController {
     private final TeacherService teacherService;
 
+    @Autowired
+	private JwtUserDetailsService userDetailsService;
+
     public TeacherController(TeacherService teacherService) {
         this.teacherService = teacherService;
     }
@@ -22,9 +29,8 @@ public class TeacherController {
     public List<TeacherModel> findAllTeachers(){
         return teacherService.findAllTeachers();
     }
-
+    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "Requestor-Type", exposedHeaders = "X-Get-Header")
     @RequestMapping(value = "/teacherDetails/{staffId}", method = RequestMethod.GET)
-    @PreAuthorize("hasRole('admin')")
     public Optional<TeacherModel> findById(@PathVariable Long staffId){
         return teacherService.findById(staffId);
     }
@@ -32,17 +38,16 @@ public class TeacherController {
     @RequestMapping(value = "/createTeacher", method = RequestMethod.POST)
     public void addTeacher(@RequestBody TeacherModel teacherModel){
         System.out.println("In controller");
+        userDetailsService.save(new UserDao(teacherModel.getStaffName(),String.valueOf(teacherModel.getPrimaryContactNumber()),"teacher"));
         teacherService.addTeacher(teacherModel);
     }
     @RequestMapping(value = "/updateTeacher/{staffId}", method = RequestMethod.PUT)
-    @PreAuthorize("hasRole('admin')")
     public void updateTeacher(@PathVariable Long staffId,@RequestBody TeacherModel teacherModel){
 
         teacherService.updateTeacherDetails(staffId,teacherModel);
     }
 
     @RequestMapping(value = "/deleteTeacher/{staffId}", method = RequestMethod.DELETE)
-    @PreAuthorize("hasRole('admin')")
     public void deleteTeacherDetails(@PathVariable Long staffId){
         teacherService.deleteTeacherDetails(staffId);
 
